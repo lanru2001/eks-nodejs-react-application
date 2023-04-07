4d3fddc2b10d57014c1d4a68ac134749360cdbae
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2023-04-06 19:32:47 -0500

    Update dev.tf

--- aws/application/environments/dev/dev.tf
+++ aws/application/environments/dev/dev.tf
@@ -1,39 +1,38 @@
 # S3 remote state 
-
 terraform {
  backend "s3" {
-    bucket         = "tf-state-file01"  
-    key            = "project/dev/ecs-app"
-    region         = "us-east-2"
-    dynamodb_table = "eks_ecommerce_dynamodb"
+    bucket         = "tf-remote-dev-bkt"  
+    key            = "project/DLFrame/eks"
+    region         = "us-east-1"
+    dynamodb_table = "DLFrame_state_locking"
 
  }
-}
+} 
 
-module "node_ecs" {
+module "dev_eks" {
+    
+    source                    = "../../"
+    public_subnets_cidr       = 
+    private_subnets_cidr      = 
+    vpc_cidr                  = 
+    create                    = true
+    name                      = 
+    namespace                 = 
+    stage                     = "dev"
+    azs                       = [
+    cloudwatch_log_group_name = "DLFrame-eks-dev"
+    cloudwatch_log_stream     = "eks"
+    name_prefix               = "DLFrame-app"
+    tag                       = "DLFrame-dev"
+    disk_size                 = 20
+    cluster_name              = "DLFrame-dev"
+    PATH_TO_PUBLIC_KEY        = "mykey.pub"
+    PATH_TO_PRIVATE_KEY       = "mykey"
+    kubernetes_version        = "1.23"
+    node_grp_name             = "DLFrame_node_group"
+    ami_id                    = "ami-08e637cea2f053dfa"
+    keypair                   = "mykeypair"
+    domain                    = "DLFrame"
+    environ                   = "dev"
 
-   source                    = "../../"
-   vpc_cidr                  = "172.31.0.0/16"
-   public_subnets_cidr       = [ "172.31.6.0/24" , "172.31.7.0/24" ]
-   private_subnets_cidr      = [ "172.31.8.0/24" , "172.31.9.0/24" ]
-   create                    = true
-   name                      = "node"
-   namespace                 = "lightfeather"
-   docker_image              = "873079457075.dkr.ecr.us-east-2.amazonaws.com/node-react-app:1.0"
-   environment               = "nodejs"
-   stage                     = "dev"
-   aws_region                = "us-east-2"
-   azs                       = ["us-east-2a" , "us-east-2b"]
-   cloudwatch_log_group_name = "ecs/lightfeather-nodejs-dev"
-   cloudwatch_log_stream     = "ecs"
-   bucket_name               = "lightfeather-lb-logs"
-   node_container_port       = "80"
-   name_prefix               = "light-app"
-   health_check_path         = "/"
-   ami_id                    = "ami-03d64741867e7bb94"
-   #public_subnet_id         = "subnet-097c6f21a3fc9e20a"
-   PATH_TO_PUBLIC_KEY        = "mykey.pub"
-   PATH_TO_PRIVATE_KEY       = "mykey"
-   tag                       = "app-dev"
- 
 }

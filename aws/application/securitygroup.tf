22b302456111fb4902d7e2d904b811a170215e0a
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2023-04-06 19:28:15 -0500

    Create securitygroup.tf

---
+++ aws/application/securitygroup.tf
@@ -0,0 +1,72 @@
+# Security group 
+resource "aws_security_group" "eks_security_group" {
+  name                      = "${local.module_prefix}-sg"
+  vpc_id                    = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    protocol    = "tcp"
+    from_port   = 3000
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   ingress {
+    protocol    = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  
+    ingress {
+    protocol    = "udp"
+    from_port   = 53
+    to_port     = 53
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+    ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    =  -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    =  -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [
+      aws_vpc.app_vpc   
+  ]
+}
